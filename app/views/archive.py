b812fef54e050a46a5c211ca7040e38ac2969726
xiidot1303 <xii1303@inbox.ru>
2025-02-26 15:09:47 +0500

    fix archiving new products

--- app/views/archive.py
+++ app/views/archive.py
@@ -19,28 +19,37 @@ class ArchiveProductView(CreateView):
         # Check if an Archive object with the same product already exists
         existing_archive = Archive.objects.filter(product=product).first()
         
+        # find product balance and minus quantity from their
+        product_balance, _ = ProductBalance.objects.get_or_create(product=product)
         if existing_archive:
-            # find product balance and minus quantity from their
-            product_balance = ProductBalance.objects.filter(product=product).first()
-            if product_balance:
-                if type == 'add':
-                    product_balance.quantity -= quantity
-                    existing_archive.quantity += quantity
-                else:
-                    product_balance.quantity += quantity
-                    existing_archive.quantity -= quantity
-                # if quantity of the product balance is less than 0, return error
-                if product_balance.quantity < 0:
-                    messages.error(self.request, 'Omborda yetarli miqdorda mahsulot yo\'q')
-                    return redirect_back(self.request)
-                if existing_archive.quantity < 0:
-                    messages.error(self.request, 'Arxivda yetarli miqdorda mahsulot yo\'q')
-                    return redirect_back(self.request)
-                product_balance.save()
+            
+            if type == 'add':
+                product_balance.quantity -= quantity
+                existing_archive.quantity += quantity
+            else:
+                product_balance.quantity += quantity
+                existing_archive.quantity -= quantity
+            # if quantity of the product balance is less than 0, return error
+            if product_balance.quantity < 0:
+                messages.error(self.request, 'Omborda yetarli miqdorda mahsulot yo\'q')
+                return redirect_back(self.request)
+            if existing_archive.quantity < 0:
+                messages.error(self.request, 'Arxivda yetarli miqdorda mahsulot yo\'q')
+                return redirect_back(self.request)
+            product_balance.save()
             # If it exists, update the quantity
             existing_archive.save()
             return redirect(self.success_url)
         else:
             # If it doesn't exist, create a new Archive object
+            product_balance.quantity -= quantity
+            # if quantity of the product balance is less than 0, return error
+            if product_balance.quantity < 0:
+                messages.error(self.request, 'Omborda yetarli miqdorda mahsulot yo\'q')
+                return redirect_back(self.request)
+            if existing_archive.quantity < 0:
+                messages.error(self.request, 'Arxivda yetarli miqdorda mahsulot yo\'q')
+                return redirect_back(self.request)
+            product_balance.save()
             return super().form_valid(form)
 
