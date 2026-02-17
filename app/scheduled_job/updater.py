4d3c82b5fab58bee1c2c8e108abec96bb3fd8ad1
xiidot1303 <xii1303@inbox.ru>
2026-02-17 09:58:52 +0500

    run jobs with newer method

--- app/scheduled_job/updater.py
+++ app/scheduled_job/updater.py
@@ -1,11 +1,23 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from django_apscheduler.jobstores import register_events, DjangoJobStore
 from app.scheduled_job import report_job
-from asgiref.sync import async_to_sync
+
+# Configure executors and sensible job defaults for production
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5),
+}
+
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 1,
+    'misfire_grace_time': 300,  # seconds
+}
 
 class jobs:
-    scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
+    scheduler = BlockingScheduler(timezone='Asia/Tashkent', executors=executors, job_defaults=job_defaults)
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     register_events(scheduler)
-    scheduler.add_job(report_job.create_daily_product_balance, 'cron', 
+    scheduler.add_job(report_job.create_daily_product_balance, 'cron',
                       hour=23, minute=40, id='create_daily_product_balance')
